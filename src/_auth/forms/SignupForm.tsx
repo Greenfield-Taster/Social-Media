4f69eb588d79bc1a1ee6a7919451fc3424ebb479
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2024-10-03 11:46:54 +0300

    Add assets folder

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/Validation";
+import { z } from "zod";
 
 const SignupForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignupForm
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+
+<div className="sm:w-240 flex-center flex-col">
+  <img src="../../../public/assets/images/logo-cropped.svg" alt="logo"/>
+</div>
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignupForm;
